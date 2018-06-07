abbffb09998b8df8c81b4ba86d386d576471adfa
huhu <bianlongting@dianwoba.com>
2018-06-07 14:34:34 +0800

    add mainnet

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,15 +1,56 @@
+var dappAddress = "n1M5ADSZx15bH13BKfPAEji1d6f6sfM2vZn";
+var nebulas = require("nebulas"),
+  Account = nebulas.Account,
+  neb = new nebulas.Neb();
+neb.setRequest(new nebulas.HttpRequest("https://mainnet.nebulas.io/"));
+//to check if the extension is installed
+//if the extension is installed, var "webExtensionWallet" will be injected in to web page
+if (typeof (webExtensionWallet) === "undefined") {
+  alert("https://github.com/ChengOrangeJu/WebExtensionWallet  is not installed, please install it first.")
+}
+
+function getAll() {
+
+  var from = Account.NewAccount().getAddressString();
+
+  var value = "0";
+  var nonce = "0"
+  var gas_price = "1000000"
+  var gas_limit = "2000000"
+  var callFunction = "getAll";
+  var callArgs = "[\"" + $("#project").val() + "\"]"; //in the form of ["args"]
+
+  var contract = {
+    "function": callFunction
+    // "args": callArgs
+  }
+
+  neb.api.call(from, dappAddress, value, nonce, gas_price, gas_limit, contract).then(function (resp) {
+    cbSearch(resp)
+  }).catch(function (err) {
+    //cbSearch(err)
+    console.log("error:" + err.message)
+  })
+}
+
+function cbSearch(resp) {
+  var result = resp.result ////resp is an object, resp.result is a JSON string
+  console.log("return of rpc call: " + JSON.stringify(result))
+}
+
+
+
 const NebPay = require('nebpay');
 const nebPay = new NebPay();
-var dappAddress = "n22fCs1yMHWbkeDw85VRrDXctpcoAYdzq2T";
 
-console.log(NebPay, nebPay);
+
+
 var serialNumber;
 
 function onButtonClick() {
   var to = dappAddress; //Dapp的合约地址
   var value = "0";
-  var callFunction = "getAll";
-
+  var callFunction = "" //调用的函数名称
   var callArgs = "" //参数格式为参数数组的JSON字符串, 比如'["arg"]','["arg1","arg2]'
   var options = {
     goods: { //商品描述
@@ -21,28 +62,11 @@ function onButtonClick() {
   }
 
   //发送交易(发起智能合约调用)
-  serialNumber = nebPay.call(to, value, callFunction, callArgs, options);
-  console.log(serialNumber, 1111111111);
-  //设置定时查询交易结果
-  // intervalQuery = setTimeout(function () {
-  //   funcIntervalQuery();
-  // }, 1000); //建议查询频率10-15s,因为星云链出块时间为15s,并且查询服务器限制每分钟最多查询10次。
+  serialNumber = nebPay.call(to, value, callFunction, callArgs, {
+    listener: cbPush
+  });
 }
 
-
-function funcIntervalQuery() {
-  //queryPayInfo的options参数用来指定查询交易的服务器地址,(如果是主网可以忽略,因为默认服务器是在主网查询)
-  nebPay.simulateCall(serialNumber) //search transaction result from server (result upload to server by app)
-    .then(function (resp) {
-      console.log("tx result: " + resp) //resp is a JSON string
-      var respObject = JSON.parse(resp)
-      //code==0交易发送成功, status==1交易已被打包上链
-      if (respObject.code === 0 && respObject.data.status === 1) {
-        //交易成功,处理后续任务....
-        clearInterval(intervalQuery) //清除定时查询
-      }
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+function cbPush(resp) {
+  console.log("response of push: " + JSON.stringify(resp))
 }
